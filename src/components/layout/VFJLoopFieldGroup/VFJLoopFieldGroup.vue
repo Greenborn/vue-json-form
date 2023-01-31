d51e09b33a38ef0f367562eb89dc1c70bf923a9d
Luciano <lucho.2012.tandil@gmail.com>
2023-01-31 19:03:05 -0300

    Mejora en modularización de los grupos de campos (loop)

--- src/components/layout/VFJLoopFieldGroup/VFJLoopFieldGroup.vue
+++ src/components/layout/VFJLoopFieldGroup/VFJLoopFieldGroup.vue
@@ -1,24 +1,19 @@
 <template>
     <div class="row">
         <div class="col">
-            <VFJButtonInput :data_channel="data_channel" :params="getCfg_new_btn()" ></VFJButtonInput>
+            <VFJButtonInput :data_channel="data_channel" :params="getCfg_new_btn()" />
         </div>
     </div>
-    <div class="row component-group" v-for="(reg_data) in list_data" :key="reg_data">
-        <div class="col">
-            <FormRow 
-                v-for="(row_data) in add_loop_references( conf, reg_data )"
-                :row_data="row_data" :data_channel="data_channel" :key="row_data"></FormRow>
-        </div>
-        <div class="col-auto">
-            <VFJButtonInput :data_channel="data_channel" :params="getCfg_remove_btn( reg_data )" ></VFJButtonInput>
-        </div>
-    </div>
+    
+    <VFJLoopFieldGroupRow v-for="(reg_data) in list_data" :key="reg_data"
+        :data_channel="data_channel" :reg_data="reg_data" :params="conf" />
+    
 </template>
 
 <script setup>
 import { ref, onMounted } from 'vue'
 import { VFJLoopFieldGroupConf } from './VFJLoopFieldGroupConf'
+import VFJLoopFieldGroupRow from './VFJLoopFieldGroupRow.vue'
 
 const props = defineProps(['params', 'data_channel'])
 
@@ -32,15 +27,6 @@ onMounted(async ()=>{
     })
 })
 
-function getCfg_remove_btn( reg_data ){
-    return {
-        action: 'list_remove_data',
-        runtime_data_field: conf.value.runtime_data_field,
-        icon: conf.value.btn_delete.icon, class: conf.value.btn_delete.class,
-        reg_data: reg_data, label: conf.value.btn_delete.label,
-    }
-}
-
 function getCfg_new_btn(){
     return {
         action: 'list_add_new_data', 
@@ -50,22 +36,4 @@ function getCfg_new_btn(){
     }
 }
 
-function add_loop_references( params, reg_data ){
-    let field = conf.value.runtime_data_field
-    let arr   = params.html_def
-
-    props.data_channel.getData('initial_values', async (init_values) => {
-        for (let i=0; i < arr.length; i++){
-            for (let j=0; j < arr[i].content.length; j++){
-                let c_params = arr[i].content[j].params
-                if (c_params['dfield'] != undefined) {
-                    c_params['field'] = field + '_' + reg_data._i + '_' + c_params.dfield
-                    c_params['value'] = init_values[ conf.value.initial_values_id ][ c_params['dfield'] ]
-                    props.data_channel.streaming('_user_input_data', { config: c_params, data: c_params['value'] })
-                }
-            }
-        }
-    })
-    return arr
-}
 </script>
